d1503c2699ebce855a20ee6f2b0011590b3681e1
Serhii Skobel <S.Skobel05@gmail.com>
2019-11-21 18:00:09 +0200

    mobile version

--- src/apps/client/ui/hocs/lang/maps/ua.js
+++ src/apps/client/ui/hocs/lang/maps/ua.js
@@ -5,6 +5,38 @@ export default {
         slider: 'Допомогти з вибором'
     },
     partners: {
-        title: 'Партнери'
+        title: 'Партнёры',
+        sections: [
+            {
+                name: 'Fusion Metal',
+                url: '/src/apps/client/ui/pages/Partners/img/fusion-metal.png',
+                alt: 'Fusion Metal',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu tincidunt eros. Duis elementum nisl sed mi sollicitudin aliquet.'
+            },
+            {
+                name: 'EMM',
+                url: '/src/apps/client/ui/pages/Partners/img/emm.png',
+                alt: 'Emm',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu tincidunt eros. Duis elementum nisl sed mi sollicitudin aliquet.'
+            },
+            {
+                name: 'Matroluxe',
+                url: '/src/apps/client/ui/pages/Partners/img/matroluxe.png',
+                alt: 'Matroluxe',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu tincidunt eros. Duis elementum nisl sed mi sollicitudin aliquet.'
+            },
+            {
+                name: 'Fusion Metal',
+                url: '/src/apps/client/ui/pages/Partners/img/fusion-metal.png',
+                alt: 'Fusion Metal',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu tincidunt eros. Duis elementum nisl sed mi sollicitudin aliquet.'
+            },
+            {
+                name: 'Matroluxe',
+                url: '/src/apps/client/ui/pages/Partners/img/matroluxe.png',
+                alt: 'Matroluxe',
+                text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu tincidunt eros. Duis elementum nisl sed mi sollicitudin aliquet.'
+            },
+        ]
     }
 };
