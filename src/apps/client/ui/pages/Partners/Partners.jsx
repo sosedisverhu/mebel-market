10e2e3ce3fa688f953ceb1536484f89907c13dbb
Serhii Skobel <S.Skobel05@gmail.com>
2019-11-21 15:58:57 +0200

    desktop version

--- src/apps/client/ui/pages/Partners/Partners.jsx
+++ src/apps/client/ui/pages/Partners/Partners.jsx
@@ -24,8 +24,17 @@ class Partners extends Component {
 
         return (
             <section className={styles.partners}>
-                <h1>{text.title}</h1>
+                <h1 className={styles.title}>{text.title}</h1>
                 <div className={styles.partnersContainer}>
+                    {text.sections.map((partner, i) =>
+                        <div className={styles.partnerItem} key={i}>
+                            <h2 className={styles.partnerName}>{partner.name}</h2>
+                            <div className={styles.partnerLogoWrapper}>
+                                <img className={styles.partnerLogo} src={partner.url} alt={partner.alt}/>
+                            </div>
+                            <p className={styles.partnerText}>{partner.text}</p>
+                        </div>
+                    )}
                 </div>
             </section>
         );
