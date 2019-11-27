aa61857259c8711bd9237eb46d71e4a5ffd1cc3b
Maks Stahnyuk <maks.stahnyuk@gmail.com>
2019-11-27 21:07:20 +0200

    add 404 section

---
+++ src/apps/client/ui/components/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import propOr from '@tinkoff/utils/object/propOr';
+
+import { connect } from 'react-redux';
+
+import styles from './NotFoundPage.css';
+
+const mapStateToProps = ({ application }) => {
+    return {
+        langMap: application.langMap,
+        langRoute: application.lang
+    };
+};
+
+class Footer extends Component {
+    static propTypes = {
+        langMap: PropTypes.object.isRequired,
+        langRoute: PropTypes.string.isRequired
+    };
+
+    render () {
+        const { langMap, langRoute } = this.props;
+        const text = propOr('notFoundPage', {}, langMap);
+
+        return <div className={styles.root}>
+            <div className={styles.contentWrap}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>404</h1>
+                    <p className={styles.text}>{text.text1}<br />{text.text2}</p>
+                    <Link to={`${langRoute}/`} className={styles.link}>{text.link}</Link>
+                </div>
+            </div>
+        </div>;
+    }
+}
+
+export default connect(mapStateToProps)(Footer);
--- src/apps/client/ui/hocs/lang/maps/ua.js
+++ src/apps/client/ui/hocs/lang/maps/ua.js
@@ -3,5 +3,10 @@
 export default {
     main: {
         slider: 'Допомогти з вибором'
+    },
+    notFoundPage: {
+        text1: 'На жаль, сторінка, яку ви шукаєте ',
+        text2: 'не існує :(',
+        link: 'На главную'
     }
 };
