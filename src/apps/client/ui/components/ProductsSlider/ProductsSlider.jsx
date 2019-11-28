d0a95c6765de8736cfa4bdc5b1cbcb86c0bb5026
Maks Stahnyuk <maks.stahnyuk@gmail.com>
2019-11-28 18:51:08 +0200

    add slider-logic

--- src/apps/client/ui/components/ProductsSlider/ProductsSlider.jsx
+++ src/apps/client/ui/components/ProductsSlider/ProductsSlider.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 
+import splitEvery from '@tinkoff/utils/array/splitEvery';
+
 import styles from './ProductsSlider.css';
 import Card from '../Card/Card';
 
@@ -12,27 +14,75 @@ class ProductsSlider extends Component {
         products: PropTypes.array.isRequired
     };
 
+    constructor (props) {
+        super(props);
+        this.state = {
+            width: 0,
+            activeIndex: 0,
+            productsInPack: 4,
+            productsPacks: []
+        };
+    }
+
+    componentDidMount () {
+        const { products } = this.props;
+        const width = this.products.clientWidth;
+        const widthWindow = window.innerWidth;
+
+        let productsNumber = 4;
+        if (widthWindow < 1361) productsNumber = 3;
+        if (widthWindow < 1021) productsNumber = 2;
+
+        const productsPacks = splitEvery(productsNumber, products);
+
+        this.setState({ width, productsPacks });
+    }
+
+    setActiveIndex = (newIndex) => {
+        const { productsPacks } = this.state;
+        const length = Math.ceil(productsPacks.length);
+
+        if (newIndex > length - 1) newIndex = 0;
+        if (newIndex < 0) newIndex = length - 1;
+
+        this.setState({ activeIndex: newIndex });
+    };
+
+    handleArrowClick = (addValue) => {
+        const { activeIndex } = this.state;
+        this.setActiveIndex(activeIndex + addValue);
+    }
+
     render () {
-        const { products, label } = this.props;
+        const { label } = this.props;
+        const { width, activeIndex, productsPacks } = this.state;
+        const left = -1 * (width * activeIndex);
 
         return (
             <div className={classNames(styles.slider, styles[label])}>
                 <div className={styles.content}>
-                    <div className={styles.products}>
-                        <div className={styles.productsPack}>
-                            {products.map(product => <Card key={product.id} product={product} />)}
-                        </div>
-                        <div className={styles.productsPack}>
-                            {products.map(product => <Card key={product.id} product={product} />)}
-                        </div>
+                    <div
+                        className={styles.products}
+                        ref={products => { this.products = products; }}
+                        style={{ left: left + 'px' }}>
+                        {productsPacks.map((products, index) => {
+                            return <div key={index} className={styles.productsPack}>
+                                {products.map(product => <Card key={product.id} product={product} />)}
+                            </div>;
+                        })}
                     </div>
                 </div>
-                <div className={styles.left} />
-                <div className={styles.right} />
+                <div className={styles.left} onClick={() => this.handleArrowClick(-1)} />
+                <div className={styles.right} onClick={() => this.handleArrowClick(1)} />
                 <div className={styles.switch}>
-                    <div className={styles.ellipse}></div>
-                    <div className={classNames(styles.ellipse, styles.active)}></div>
-                    <div className={styles.ellipse}></div>
+                    {productsPacks.map((products, index) => {
+                        return (
+                            <div
+                                key={index}
+                                onClick={() => this.setActiveIndex(index)}
+                                className={classNames(styles.ellipse, { [styles.active]: index === activeIndex })}>
+                            </div>);
+                    })}
                 </div>
             </div>);
     }
--- src/apps/client/ui/pages/MainPage/MainPage.jsx
+++ src/apps/client/ui/pages/MainPage/MainPage.jsx
@@ -1,15 +1,66 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import propOr from '@tinkoff/utils/object/propOr';
+import includes from '@tinkoff/utils/array/includes';
 import { connect } from 'react-redux';
 
-import Slider from '../../components/Slider/Slider';
+import { MAX_NEW_PROSUCTS } from '../../../constants/constants';
+
+import styles from './MainPage.css';
+import ProductsSlider from '../../components/ProductsSlider/ProductsSlider';
+
+const mapStateToProps = ({ application, data }) => {
+    return {
+        langMap: application.langMap,
+        products: data.products,
+        labels: data.labels
+    };
+};
 
 class MainPage extends Component {
+    static propTypes = {
+        langMap: PropTypes.object.isRequired,
+        labels: PropTypes.array.isRequired,
+        products: PropTypes.array.isRequired
+    };
+
     render () {
+        const { langMap, products, labels } = this.props;
+        const text = propOr('mainPage', {}, langMap);
+
+        const productsResult = products
+            .sort((prev, next) => next.date - prev.date)
+            .reduce((result, product) => {
+                if (includes('top', product.labels)) {
+                    (result.top) ? result.top.push(product) : result.top = [product];
+                }
+
+                if (product.discountPrice) {
+                    (result.discount) ? result.discount.push(product) : result.discount = [product];
+                }
+
+                if (result.new) {
+                    if (result.new.length < MAX_NEW_PROSUCTS) {
+                        result.new.push(product);
+                    }
+                } else {
+                    result.new = [product];
+                }
+
+                return result;
+            }, {});
+
         return (
-            <Slider />
-        );
+            <div>
+                {labels.map(label => {
+                    return (
+                        <section key={label} className={styles.categorySection}>
+                            <h2 className={styles.title}>{text[label]}</h2>
+                            <ProductsSlider label={label} products={productsResult[label]} />
+                        </section>);
+                })}
+            </div>);
     }
 }
 
-export default connect()(MainPage);
+export default connect(mapStateToProps)(MainPage);
