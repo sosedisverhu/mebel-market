3cd1287d9c7b494fe6b5cb44e476fc53b7beb864
Maks Stahnyuk <maks.stahnyuk@gmail.com>
2019-11-29 13:43:20 +0200

    fix lang params

--- src/apps/client/ui/components/MainCategories/MainCategories.jsx
+++ src/apps/client/ui/components/MainCategories/MainCategories.jsx
@@ -1,39 +1,55 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import propOr from '@tinkoff/utils/object/propOr';
 
 import styles from './MainCategories.css';
 
+const mapStateToProps = ({ application }) => {
+    return {
+        langMap: application.langMap
+    };
+};
+
 class MainCategories extends Component {
+    static propTypes = {
+        langMap: PropTypes.object.isRequired
+    };
+
     render () {
+        const { langMap } = this.props;
+        const text = propOr('mainPage', {}, langMap);
+
         return <div className={styles.categoriesWrap}>
             <div className={styles.categories}>
                 <a className={styles.category} href='#'>
                     <div className={styles.imgWrap}>
-                        <img className={styles.img} src="/src/apps/client/ui/components/MainCategories/img/category1.jpg" alt="" />
+                        <img className={styles.img} src="/src/apps/client/ui/components/MainCategories/img/category1.jpg" alt={text.bedsTitle} />
                     </div>
-                    <h3 className={styles.title}>Кровати</h3>
+                    <h3 className={styles.title}>{text.bedsTitle}</h3>
                 </a>
                 <a className={styles.category} href='#'>
                     <div className={styles.imgWrap}>
-                        <img className={styles.img} src="/src/apps/client/ui/components/MainCategories/img/category2.jpg" alt="" />
+                        <img className={styles.img} src="/src/apps/client/ui/components/MainCategories/img/category2.jpg" alt={text.mattressesTitle} />
                     </div>
-                    <h3 className={styles.title}>Матрасы</h3>
+                    <h3 className={styles.title}>{text.mattressesTitle}</h3>
                 </a>
                 <a className={styles.category} href='#'>
                     <div className={styles.imgWrap}>
-                        <img className={styles.img} src="/src/apps/client/ui/components/MainCategories/img/category3.jpg" alt="" />
+                        <img className={styles.img} src="/src/apps/client/ui/components/MainCategories/img/category3.jpg" alt={text.furnitureTitle} />
                     </div>
-                    <h3 className={styles.title}>Мягкая мебель </h3>
+                    <h3 className={styles.title}>{text.furnitureTitle}</h3>
                 </a>
                 <a className={styles.category} href='#'>
                     <div className={styles.imgWrap}>
-                        <img className={styles.img} className={styles.img} src="/src/apps/client/ui/components/MainCategories/img/category4.jpg" alt="" />
+                        <img className={styles.img} src="/src/apps/client/ui/components/MainCategories/img/category4.jpg" alt={text.accessoriesTitle} />
                     </div>
-                    <h3 className={styles.title}>Аксессуары для сна</h3>
+                    <h3 className={styles.title}>{text.accessoriesTitle}</h3>
                 </a>
             </div>
         </div>;
     }
 }
 
-export default connect()(MainCategories);
+export default connect(mapStateToProps)(MainCategories);
