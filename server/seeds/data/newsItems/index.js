3160c9492fce383c7d033e06b451a4854d6f530c
Pavel Matsenko <pavel2mac@gmail.com>
2019-12-02 15:32:12 +0200

    added seeds

---
+++ server/seeds/data/newsItems/index.js
@@ -0,0 +1,128 @@
+/* eslint-disable max-len */
+
+module.exports = [
+    {
+        id: '1',
+        alias: 'news1',
+        texts: {
+            ru: {
+                name: 'новость1',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' +
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
+                    ' occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            ua: {
+                name: 'новина1',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' +
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
+                    ' occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
+        },
+        avatar: '/server/seeds/data/products/images/Prague.png',
+        files: ['/server/seeds/data/products/images/Prague.png',
+            '/server/seeds/data/products/images/Prague.png',
+            '/server/seeds/data/products/images/Prague.png'],
+        hidden: false,
+        date: 1575210168762,
+        youTubeVideo: 'http://youtube.com'
+    },
+    {
+        id: '2',
+        alias: 'news2',
+        texts: {
+            ru: {
+                name: 'новость2',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' +
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
+                    ' occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            ua: {
+                name: 'новина2',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' +
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
+                    ' occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
+        },
+        avatar: '/server/seeds/data/products/images/Glory.png',
+        files: ['/server/seeds/data/products/images/Glory.png',
+            '/server/seeds/data/products/images/Glory.png',
+            '/server/seeds/data/products/images/Glory.png'],
+        hidden: false,
+        date: 1575210168772,
+        youTubeVideo: 'http://youtube.com'
+    },
+    {
+        id: '3',
+        alias: 'news3',
+        texts: {
+            ru: {
+                name: 'новость3',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' +
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
+                    ' occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            ua: {
+                name: 'новина3',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' +
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
+                    ' occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
+        },
+        avatar: '/server/seeds/data/products/images/Classik.png',
+        files: ['/server/seeds/data/products/images/Classik.png',
+            '/server/seeds/data/products/images/Classik.png',
+            '/server/seeds/data/products/images/Classik.png'],
+        hidden: false,
+        date: 1575210168777,
+        youTubeVideo: 'http://youtube.com'
+    },
+    {
+        id: '4',
+        alias: 'news4',
+        texts: {
+            ru: {
+                name: 'новость4',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' +
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
+                    ' occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            },
+            ua: {
+                name: 'новина3',
+                description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,' +
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi' +
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit' +
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint' +
+                    ' occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            }
+        },
+        avatar: '/server/seeds/data/products/images/Antalya.png',
+        files: ['/server/seeds/data/products/images/Antalya.png',
+            '/server/seeds/data/products/images/Antalya.png',
+            '/server/seeds/data/products/images/Antalya.png'],
+        hidden: false,
+        date: 1575210168778,
+        youTubeVideo: 'http://youtube.com'
+    }
+];
