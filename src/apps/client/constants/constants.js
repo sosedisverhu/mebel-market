6c52d7217faf27393bf86e8f980001e45839f6b6
Serhii Skobel <S.Skobel05@gmail.com>
2019-12-02 15:56:28 +0200

    add pagination

--- src/apps/client/constants/constants.js
+++ src/apps/client/constants/constants.js
@@ -1,4 +1,3 @@
 export const LANGS = ['ru', 'ua']; // need to add lang maps
 export const DEFAULT_LANG = LANGS[0];
 export const DEFAULT_LANG_ROUTE = '';
-export const MAX_ARTICLES = 6;
--- src/apps/client/ui/pages/Articles/Articles.jsx
+++ src/apps/client/ui/pages/Articles/Articles.jsx
@@ -5,29 +5,50 @@ import { Link } from 'react-router-dom';
 import propOr from '@tinkoff/utils/object/propOr';
 import styles from './Articles.css';
 
-const mapStateToProps = ({ application, data }) => {
+import Pagination from '../../components/Pagination/Pagination.jsx';
+
+const mapStateToProps = ({ application }) => {
     return {
-        langMap: application.langMap,
-        lang: application.lang,
-        articles: data.articles
+        langMap: application.langMap
     };
 };
 
 class Articles extends Component {
     static propTypes = {
-        langMap: PropTypes.object.isRequired,
-        lang: PropTypes.string.isRequired
+        langMap: PropTypes.object.isRequired
     };
 
+    state = {
+        currentPage: 1,
+        postsPerPage: 6
+    }
+
+    paginate = (pageNumber) => this.setState({currentPage: Number(pageNumber.target.id)});
+
     render () {
-        const { langMap, lang, articles } = this.props;
+        const { langMap } = this.props;
+        const { currentPage, postsPerPage } = this.state;
         const text = propOr('articles', {}, langMap);
+
+        const indexOfLastPost = currentPage * postsPerPage;
+        const indexOfFirstPost = indexOfLastPost - postsPerPage;
+
+        const previousPage = (pageNumber) => {
+            if (currentPage > 1) {
+                this.setState({currentPage: Number(pageNumber.target.id) - 1});
+            }
+        };
+
+        const nextPage = (pageNumber) => {
+            if (currentPage < text.sections.length / postsPerPage) {
+                this.setState({currentPage: Number(pageNumber.target.id) + 1});
+            }
+        };
 
         return (
             <section className={styles.articles}>
                 <div className={styles.articlesContainer}>
-                    {text.sections.map(article =>
-                        [...Array(3)].map(e =>
+                    {text.sections.slice(indexOfFirstPost, indexOfLastPost).map(article =>
                         <div className={styles.article} key={article.id}>
                             <Link className={styles.titleLink} to={`/articles/${article.id}`}>
                                 <h1 className={styles.title}><span className={styles.titleUnderline}>{article.title}</span></h1>
@@ -40,7 +61,17 @@ class Articles extends Component {
                                 <span className={styles.date}>{article.date}</span>
                             </div>
                         </div>
-                    ))}
+                    )}
+                    {text.sections.length > postsPerPage &&
+                        <Pagination
+                            postsPerPage={postsPerPage}
+                            totalPosts={text.sections.length}
+                            currentPage={currentPage}
+                            paginate={this.paginate}
+                            previousPage={previousPage}
+                            nextPage={nextPage}
+                        />
+                    }
                 </div>
             </section>
         );
