ba2f765e7ec29e71522634b646eb163d1a8b74f1
Maks Stahnyuk <maks.stahnyuk@gmail.com>
2019-12-05 11:20:31 +0200

    save

--- server/api/client/article/model.js
+++ server/api/client/article/model.js
@@ -3,15 +3,6 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const Article = new Schema({
-    // id: { type: String, required: true },
-    // texts: { type: Object, required: true },
-    // avatar: { type: String },
-    // files: [{ type: String, required: true }],
-    // youTubeVideo: { type: String },
-    // hidden: { type: Boolean, required: true },
-    // date: { type: Number, required: true },
-    // alias: { type: String, required: true }
-
     id: { type: String },
     texts: { type: Object },
     hidden: { type: Boolean },
