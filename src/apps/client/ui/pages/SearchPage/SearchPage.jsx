ce2772ee073831b0744921b8d0c3ccb745551c20
Serhii Skobel <S.Skobel05@gmail.com>
2019-12-11 18:03:53 +0200

    save

--- src/apps/client/ui/pages/SearchPage/SearchPage.jsx
+++ src/apps/client/ui/pages/SearchPage/SearchPage.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
-
-import { Link } from 'react-router-dom';
+import searchByText from '../../../services/client/searchByText';
+import { Link, withRouter } from 'react-router-dom';
 
 import propOr from '@tinkoff/utils/object/propOr';
-
+import queryString from 'query-string';
 // import ProductsGrid from '../../components/ProductsGrid/ProductsGrid';
 
 import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs.jsx';
@@ -16,15 +16,25 @@ import Sort from '../../components/Sort/Sort';
 
 const mapStateToProps = ({ application, data }) => {
     return {
+        langRoute: application.langRoute,
         langMap: application.langMap,
+        lang: application.lang,
         products: data.products,
-        labels: data.labels
+        labels: data.labels,
+        articles: data.articles
     };
 };
 
+const mapDispatchToProps = (dispatch) => ({
+    searchByText: payload => dispatch(searchByText(payload))
+});
+
 class SearchPage extends Component {
     static propTypes = {
-        langMap: PropTypes.object.isRequired
+        langRoute: PropTypes.string.isRequired,
+        langMap: PropTypes.object.isRequired,
+        lang: PropTypes.string.isRequired,
+        searchByText: PropTypes.func.isRequired
     };
 
     constructor (...args) {
@@ -32,14 +42,58 @@ class SearchPage extends Component {
 
         this.state = {
             products: [],
-            searchText: 'Кровати металлические'
+            articles: [],
+            searchText: '',
+            loading: true
         };
     }
 
+    // componentDidMount () {
+    //     const { location: { searchText } } = this.props;
+    //     const query = queryString.parse(searchText);
+    //
+    //     this.searchByText(query.text)
+    //         .then(() => {
+    //             this.setState({
+    //                 loading: false
+    //             });
+    //         });
+    // }
+    //
+    // searchByText (text) {
+    //     return this.props.searchByText(text)
+    //     .then(({ products, articles }) => {
+    //         this.setState({
+    //             products,
+    //             articles,
+    //             searchedText: text
+    //         });
+    //     });
+    // }
+    //
+    // handleInputSubmit = inputValue => {
+    //     e.preventDefault();
+    //     if (inputValue) {
+    //         const { langRoute } = this.props;
+    //
+    //         this.searchByText(inputValue)
+    //         .then(() => {
+    //             this.props.history.push(`${langRoute}/search?text=${inputValue}`);
+    //         });
+    //     }
+    // };
+
+
     render () {
-        const { langMap } = this.props;
-        const { products, searchText } = this.state;
+        const { langMap, lang } = this.props;
+        const { products, articles, searchText, loading } = this.state;
         const text = propOr('searchPage', {}, langMap);
+
+        if (loading) {
+            return <div className={styles.loader}>
+                <img src='/src/apps/client/ui/pages/SearchPage/img/loader.svg' alt='loader'/>
+            </div>;
+        }
 
         return (
             <section className={styles.search}>
@@ -61,9 +115,15 @@ class SearchPage extends Component {
                             <div className={styles.searchNotFound}>
                                 <h2 className={styles.searchNotFoundText}>{`${text.noResults} “${searchText}”`}</h2>
                                 <h3 className={styles.tryAgain}>{text.tryAgain}</h3>
-                                <form className={styles.form} action="">
+                                <form className={styles.form} onSubmit={this.handleSearchSubmit}>
                                     <div className={styles.searchInputWrap}>
-                                        <input className={styles.searchInput} type="text" placeholder={text.placeholder} />
+                                        <input
+                                            className={styles.searchInput}
+                                            type="text"
+                                            placeholder={text.placeholder}
+                                            value={this.state.searchText}
+                                            onChange={this.handleInputChange}
+                                        />
                                     </div>
                                     <button className={styles.searchButton} type="submit"></button>
                                 </form>
@@ -77,4 +137,4 @@ class SearchPage extends Component {
     }
 }
 
-export default connect(mapStateToProps)(SearchPage);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(SearchPage));
