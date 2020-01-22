c0264db1514e595152667c9a450ce2efc2e7b46d
MStahnyuk <33137377+MStahnyuk@users.noreply.github.com>
2020-01-22 15:42:10 +0200

    add draggable product gallery

--- src/apps/client/ui/components/Gallery/Gallery.jsx
+++ src/apps/client/ui/components/Gallery/Gallery.jsx
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
-import styles from './Gallery.css';
+import styles from './GalleryPhotos.css';
+import Draggable from '../Draggable/Draggable';
 
-class Gallery extends Component {
+const IGNORE_SWIPE_DISTANCE = 50;
+
+const mapStateToProps = ({ application }) => {
+    return {
+        mediaWidth: application.media.width
+    };
+};
+
+class GalleryPhotos extends Component {
     static propTypes = {
         photos: PropTypes.array.isRequired,
+        mediaWidth: PropTypes.number.isRequired,
+        location: PropTypes.object.isRequired,
         discount: PropTypes.number
     };
 
@@ -16,21 +27,141 @@ class Gallery extends Component {
         discount: 0
     };
 
-    state = {
-        activeImg: 0
+    constructor (...args) {
+        super(...args);
+
+        this.maxSlideIndex = this.props.photos.length - 1;
+        this.photoSliderTrack = React.createRef();
+        this.state = {
+            activeImg: 0,
+            activeSlideIndex: 0,
+            sliderLeft: 0,
+            sliderWidth: 0
+        };
+    }
+
+    maxSlide = this.props.photos.length - 1;
+    maxLeft = this.maxSlide * this.props.mediaWidth;
+
+    handleDragStart = () => {
+        this.startLeft = this.state.sliderLeft;
     };
 
-    handleImgClick = index => this.setState({ activeImg: index });
+    handleDragProcess = ({ delta: { client: { x: deltaX } } }) => {
+        const nextSliderLeft = this.startLeft - deltaX;
+        const { activeSlideIndex } = this.state;
+
+        if (nextSliderLeft < 0 || nextSliderLeft > this.maxLeft || activeSlideIndex === this.maxSlideIndex) return;
+
+        this.setState({
+            sliderLeft: this.startLeft - deltaX
+        });
+
+        this.photoSliderTrack.current.style.transition = `none`;
+        this.photoSliderTrack.current.style.left = `-${this.startLeft - deltaX}px`;
+    };
+
+    handleDragEnd = ({ delta: { client: { x: deltaX } } }) => {
+        const { photos } = this.props;
+        const { activeSlideIndex, sliderWidth } = this.state;
+        const nextActiveSlideIndex = deltaX > 0 ? activeSlideIndex - 1 : activeSlideIndex + 1;
+
+        if (Math.abs(deltaX) < IGNORE_SWIPE_DISTANCE || nextActiveSlideIndex === -1 || nextActiveSlideIndex === photos.length) {
+            return this.setState({
+                sliderLeft: sliderWidth * activeSlideIndex,
+                activeImg: activeSlideIndex
+            });
+        }
+
+        this.startLeft = null;
+        this.setState({
+            activeSlideIndex: nextActiveSlideIndex,
+            sliderLeft: nextActiveSlideIndex <= this.maxSlideIndex
+                ? sliderWidth * nextActiveSlideIndex
+                : 0,
+            activeImg: nextActiveSlideIndex
+        });
+
+        this.photoSliderTrack.current.style.transition = `left .5s ease-in-out`;
+        this.photoSliderTrack.current.style.left = `-${sliderWidth * nextActiveSlideIndex}px`;
+    };
+
+    handleImgClick = (index) => {
+        const { sliderWidth } = this.state;
+        this.setState({ activeImg: index, activeSlideIndex: index, sliderLeft: index * sliderWidth });
+        this.photoSliderTrack.current.style.left = `-${index * sliderWidth}px`;
+    };
+
+    handleArrowClick = direction => () => {
+        const { activeImg, sliderWidth } = this.state;
+        const newIndex = direction === 'left' ? activeImg - 1 : activeImg + 1;
+
+        this.setState({
+            activeImg: newIndex,
+            sliderLeft: newIndex * sliderWidth
+        });
+
+        this.photoSliderTrack.current.style.left = `-${newIndex * sliderWidth}px`;
+    };
+
+    componentWillReceiveProps (nextProps) {
+        if (nextProps.mediaWidth !== this.props.mediaWidth) {
+            const { activeSlideIndex, sliderWidth } = this.state;
+
+            this.setSliderWidth();
+            this.maxLeft = sliderWidth * this.maxSlide;
+            this.setState({
+                sliderLeft: sliderWidth * activeSlideIndex,
+                activeImg: activeSlideIndex
+            });
+
+            this.photoSliderTrack.current.style.left = `-${activeSlideIndex * sliderWidth}px`;
+        }
+
+        if (this.props.location !== nextProps.location || this.props.location.pathname !== nextProps.location.pathname) {
+            this.setState({
+                activeImg: 0,
+                activeSlideIndex: 0,
+                sliderLeft: 0
+            });
+        }
+    }
+
+    setSliderWidth = () => {
+        this.setState({ sliderWidth: this.photoSliderTrack.current.clientWidth });
+    }
 
     render () {
         const { photos, discount } = this.props;
         const { activeImg } = this.state;
+
         return (
             <div className={styles.gallery}>
                 {discount ? <div className={styles.discount}>{discount}<span className={styles.percentage}>%</span></div> : null}
-                <div>
-                    <img className={styles.mainImg} src={photos[activeImg]} width="681" height="400" alt="main image" />
-                </div>
+                <Draggable
+                    onDragStart={this.handleDragStart}
+                    onDrag={this.handleDragProcess}
+                    onDragEnd={this.handleDragEnd}
+                    allowDefaultAction
+                    touchable
+                >
+                    <div className={styles.mainImgWrap}>
+                        <div className={styles.slider}
+                            ref={this.photoSliderTrack}>
+                            {
+                                photos.map((img, i) => {
+                                    return (
+                                        <img className={styles.mainImg}
+                                            src={img}
+                                            alt="main image"
+                                            onLoad={this.setSliderWidth}
+                                            key={i}/>
+                                    );
+                                })
+                            }
+                        </div>
+                    </div>
+                </Draggable>
                 {photos.length > 1 && <div className={styles.additionalImgs}>
                     {
                         photos.map((img, index) => {
@@ -50,9 +181,8 @@ class Gallery extends Component {
                     }
                 </div>}
             </div>
-
         );
     }
 }
 
-export default connect()(Gallery);
+export default withRouter(connect(mapStateToProps)(GalleryPhotos));
