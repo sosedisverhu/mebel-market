f95195d72c43c3710831644c42b2c7857e62be9e
v.bilous <v.bilous.upwork@gmail.com>
2020-07-26 13:51:43 +0300

    почта

--- server/api/client/form/queries/contactApplicationQuery.js
+++ server/api/client/form/queries/contactApplicationQuery.js
@@ -3,8 +3,7 @@ import nodemailer from 'nodemailer';
 const LOGIN = process.env.LOGIN || 'mebelmarket123@gmail.com';
 const PASS = process.env.PASS || 'Qweqwe123.';
 const SENDER = process.env.SENDER || 'МебельМаркет <mebelmarket123@gmail.com>';
-// const RECEIVER = process.env.RECEIVER || 'maxiv4545@gmail.com';
-const RECEIVER = process.env.RECEIVER || 'v.bilous14@gmail.com';
+const RECEIVER = process.env.RECEIVER || 'maxiv4545@gmail.com';
 
 export default function contactApplication (subject, content, files, successCallback, failureCallback) {
     const transporter = nodemailer.createTransport({
