68ec0afacfcf751641b7276b8065c65c6704d3d1
VictoriaVM <victoria.v.movchan@gmail.com>
2021-06-09 16:59:05 +0300

    features in product card admin

--- src/apps/admin/constants/constants.js
+++ src/apps/admin/constants/constants.js
@@ -7,5 +7,16 @@ export const FEATURE_TYPES = [
     { value: '3', label: 'Механизм', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/detail.png' },
     { value: '4', label: 'Оплата', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/sertificat.png' },
     { value: '5', label: 'Гарантия', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/warranty.png' },
-    { value: '6', label: 'Доставка', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/delivery.png' }
+    { value: '6', label: 'Доставка', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/delivery.png' },
+    { value: '7', label: 'Дерево', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/wood.png' },
+    { value: '8', label: 'Зоны', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/zones.png' },
+    { value: '9', label: 'Кровать', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/bed.png' },
+    { value: '10', label: 'Слои', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/layers.png' },
+    { value: '11', label: 'Кокос', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/coconut.png' },
+    { value: '12', label: 'Еко', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/eco.png' },
+    { value: '13', label: 'Карточки', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/labels.png' },
+    { value: '13', label: 'Температура', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/cold.png' },
+    { value: '14', label: 'Вес', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/weight.png' },
+    { value: '15', label: 'Толщина', photo: '/src/apps/admin/ui/components/Form/fields/FormFieldFeature/img/thick.png' },
+
 ];
